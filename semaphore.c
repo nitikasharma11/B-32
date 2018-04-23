149f869b4571f5780955302f34fd202c6518dae8
nitikasharma11 <38591579+nitikasharma11@users.noreply.github.com>
2018-04-24 00:41:05 +0530

    Update semaphore.c

--- semaphore.c
+++ semaphore.c
@@ -1,38 +1,52 @@
-#include<pthread.h>
-#include<stdio.h>
-#include<semaphore.h>
-void *fun1();
-void *fun2();
-int shared=1;
-sem_t s;
-int main()
-{
-sem_init(&s,0,1);
-pthread_t thread1,thread2;
-pthread_create(&thread1,NULL,fun1,NULL);
-pthread_create(&thread2,NULL,fun2,NULL);
-pthread_join(thread1,NULL);
-pthread_join(thread2,NULL);
-printf("Final Value of shared is %d\n",shared);
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+void *thread_function(void *arg);
+sem_t bin_sem;
+
+#define WORK_SIZE 1024
+char work_area[WORK_SIZE];
+
+int main() {
+    int res;
+    pthread_t a_thread;
+    void *thread_result;
+
+    res = sem_init(&bin_sem, 0, 0);
+    if (res != 0) {
+        perror("Semaphore initialization failed");
+        exit(EXIT_FAILURE);
+    }
+    res = pthread_create(&a_thread, NULL, thread_function, NULL);
+    if (res != 0) {
+        perror("Thread creation failed");
+        exit(EXIT_FAILURE);
+    }
+    printf("Input some text. Enter 'end' to finish\n");
+    while(strncmp("end", work_area, 3) != 0) {
+        fgets(work_area, WORK_SIZE, stdin);
+        sem_post(&bin_sem);
+    }
+    printf("\nWaiting for thread to finish...\n");
+    res = pthread_join(a_thread, &thread_result);
+    if (res != 0) {
+        perror("Thread join failed");
+        exit(EXIT_FAILURE);
+    }
+    printf("Thread joined\n");
+    sem_destroy(&bin_sem);
+    exit(EXIT_SUCCESS);
 }
 
-void *fun1()
-{
-int x;
-sem_wait(&s);
-x=shared;
-x++;
-sleep(1);
-shared=x;
-sem_post(&s);
-}
-void *fun2()
-{
-int x;
-sem_wait(&s);
-x=shared;
-x--;
-sleep(1);
-shared=x;
-sem_wait(&s);
-}
+void *thread_function(void *arg) {
+    sem_wait(&bin_sem);
+    while(strncmp("end", work_area, 3) != 0) {
+        printf("You input %d characters\n", strlen(work_area) -1);
+        sem_wait(&bin_sem);
+    }
+    pthread_exit(NULL);
+}  
